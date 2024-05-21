881ef16c969ad5a1893f3b329afa2b770abd3fc5
ayeshamahar12 <ayeshamahar007@gmail.com>
2024-05-21 13:15:57 +0500

    hello

---
+++ index.js
@@ -0,0 +1,94 @@
+ connum=77;
+ num=776;
+num=33;
+console.log(num);
+
+
+// let  num="44";
+// let  num2="66";
+
+// alert(num+num2);
+  
+// console.log(num+num2)
+
+//  let  firstname="muhammad";
+// let lestname="shohaib";
+// // // console.log(firstname +" "+ lastname);
+//  console.log(firstName + " " + lastname);
+
+
+// Number +  Number addition 
+// // num  + String  concatenation ;
+//  num + string concatenation 
+//  string + Number concatenation;
+
+// let email="asd@gamil.com"
+// let name ="abdullah"
+// let account="2369025487900"
+// let  etc
+// console.table([email,name,account,etc]);
+// / math operators  =  +  -  *  / divided % modules
+
+
+//  let  num="66";
+// let  num2="1";
+
+
+//  console.log(num+num2);
+
+
+// let  data="100";
+// let  data2="5";
+
+
+// // //  console.log(data+data2);
+// let name = rompt("what is  your name........................")
+// document.write(name)
+
+
+// letname = rompt("what is  your number.....................")
+// console.log(name)
+
+// let rendomNumbwer1 = Math.random();
+//  console.log(rendomNumbwer1)
+
+// let rendomNumbwer = Math.random();
+// rendomNumbwer = rendomNumbwer * 10; 
+//  let rounded = Math.round(rendomNumbwer)
+// console.log(rounded)
+//  BODMAS  =  BARKET  OPEN DIVIDED MULPLY ADD SUBTRA 
+// let num = 4 + 6 * (5 + 5) ;
+// console.log(num);
+
+// let x= 33;
+// console.log(x);
+// console.log(typeof String(x) );
+
+// let name =  null;
+// consol.log(name);
+
+// Boolean;
+
+let x = 33;
+ let y =false;
+ console.log(typeof y);
+ console.log( x+(y));
+
+letx =33;
+ lety = true;
+console.log(x+ 12 + y);
+
+
+
+
+//   let x = 33;
+//  let y = false;
+
+//  console.log(typeof y);
+
+// console.log(x + y)
+
+
+//  letx = 33; 
+//  lety = true ;
+//  console.log(x + 12 + y);
